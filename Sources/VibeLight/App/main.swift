3e093dec23bd64c41aeead8dd2c41b85901dd6dd
YM <ymfu@hku.hk>
2026-03-26 18:28:54 +0800

    Set menu bar app activation policy

--- Sources/VibeLight/App/main.swift
+++ Sources/VibeLight/App/main.swift
@@ -1,6 +1,12 @@
 import AppKit
 
+@MainActor
+func configureApplication(_ app: NSApplication, delegate: AppDelegate) {
+    app.setActivationPolicy(.accessory)
+    app.delegate = delegate
+}
+
 let app = NSApplication.shared
 let delegate = AppDelegate()
-app.delegate = delegate
+configureApplication(app, delegate: delegate)
 app.run()
--- Tests/VibeLightTests/ScaffoldSmokeTests.swift
+++ Tests/VibeLightTests/ScaffoldSmokeTests.swift
@@ -1,7 +1,22 @@
+import AppKit
 import Testing
 @testable import VibeLight
 
 @Test
 func scaffoldCompiles() {
     #expect(Bool(true))
+}
+
+@MainActor
+@Test
+func configuresMenuBarApplicationActivationPolicy() {
+    let app = NSApplication.shared
+    _ = app.setActivationPolicy(.regular)
+
+    let delegate = AppDelegate()
+
+    configureApplication(app, delegate: delegate)
+
+    #expect(app.activationPolicy() == .accessory)
+    #expect(app.delegate === delegate)
 }
